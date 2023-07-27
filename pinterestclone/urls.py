3d9785c43c3524e28697121d90e0c9a6d98cd1b2
EarthCaspian <hazar.a@outlook.com>
2023-07-27 23:33:02 +0300

    login ve register html ve url pathleri eklendi

--- pinterestclone/urls.py
+++ pinterestclone/urls.py
@@ -17,11 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from pinterestmain.views import *
+from user.views import *
 
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pinterestmain.urls'))
+    path('', include('pinterestmain.urls')),
+    path('register/', userRegister, name='register'),
+    path('login/', userLogin, name='login')
 ]  + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
