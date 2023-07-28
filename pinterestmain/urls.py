0ec3eb89b221716b4021f1b0c87c6864580d2fbc
EarthCaspian <hazar.a@outlook.com>
2023-07-28 15:29:32 +0300

    pins views yazıldı

--- pinterestmain/urls.py
+++ pinterestmain/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from pinterestmain.views import *
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('pins/', pinsDetail, name='pins')
 ]
