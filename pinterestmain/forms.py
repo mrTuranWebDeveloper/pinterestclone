c75f4a96976478d590855e782aa70e394f57418e
EarthCaspian <hazar.a@outlook.com>
2023-08-05 14:47:08 +0300

    IdeaPinForm olusturuldu, Pinform güncellendi

--- pinterestmain/forms.py
+++ pinterestmain/forms.py
@@ -4,13 +4,31 @@ from .models import *
 class PinForm(ModelForm):
     class Meta:
         model = Pin
-        fields = ['title', 'description', 'image']
+        fields = ['title', 'description', 'image', 'url', 'board']
         labels = {
             'title': 'Başlık',
             'description': 'Açıklama',
-            'image': 'Görsel'
+            'image': 'Görsel',
+            'url': 'Bağlantı',
+            'board': 'Pano'
         }
     def __init__(self, *args, **kwargs):
         super(PinForm, self).__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class':'form-control'})
+
+class IdeaPinForm(ModelForm):
+    class Meta:
+        model = IdeaPin
+        fields = ['title', 'description', 'image','url','board']
+        labels = {
+            'title': 'Başlık',
+            'description': 'Açıklama',
+            'image': 'Görsel',
+            'url': 'Bağlantı',
+            'board': 'Pano'
+        }
+    def __init__(self,*args,**kwargs):
+        super(IdeaPinForm,self).__init__(*args,**kwargs)
         for name, field in self.fields.items():
             field.widget.attrs.update({'class':'form-control'})
