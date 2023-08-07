4cd55db50ca936f5f934bc7c0d05569883998130
EarthCaspian <hazar.a@outlook.com>
2023-08-07 16:26:06 +0300

    ideapins url path eklendi

--- pinterestmain/urls.py
+++ pinterestmain/urls.py
@@ -4,6 +4,7 @@ from pinterestmain.views import *
 urlpatterns = [
     path('', index, name='index'),
     path('pins/<int:pinId>', pinsDetail, name='pins'),
+    path('ideapins/<int:ideapinId>', idea_pins_Detail, name='ideapins'),
     path('create_pin/', create_pin, name='create_pin'),
     path('create_ideapin/', create_idea_pin, name='create_idea_pin'),
     path('homepage/', homepage, name='homepage'),
