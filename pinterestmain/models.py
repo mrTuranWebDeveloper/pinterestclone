a1e72012b8189c81b3326ea7445c5536a8131228
EarthCaspian <hazar.a@outlook.com>
2023-08-10 16:54:04 +0300

    comment textfield karakter sınırı eklendi

--- pinterestmain/models.py
+++ pinterestmain/models.py
@@ -77,7 +77,7 @@ class Comment(models.Model):
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
     commenter = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
+    content = models.TextField(max_length=500)
 
     created_at = models.DateTimeField(auto_now_add=True)
 
