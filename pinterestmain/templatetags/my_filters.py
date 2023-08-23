748e37b02bc01607feb7c2eaf0bbd9f5f9e57f73
EarthCaspian <hazar.a@outlook.com>
2023-08-23 21:20:31 +0300

    yorum zaman göstergesi icin filtreleme fonksiyonu

---
+++ pinterestmain/templatetags/my_filters.py
@@ -0,0 +1,30 @@
+from django import template
+from django.utils import timezone
+
+register = template.Library()
+
+@register.filter
+def compact_naturaltime(value):
+    now = timezone.now()
+    if value > now:
+        delta = value - now
+        delta_seconds = delta.total_seconds()
+        if delta_seconds < 60:
+            return f"{int(delta_seconds)}s"
+        elif delta_seconds < 3600:
+            return f"{int(delta_seconds / 60)}m"
+        elif delta_seconds < 86400:
+            return f"{int(delta_seconds / 3600)}h"
+        else:
+            return f"{int(delta_seconds / 86400)}d"
+    else:
+        delta = now - value
+        delta_seconds = delta.total_seconds()
+        if delta_seconds < 60:
+            return f"{int(delta_seconds)}s"
+        elif delta_seconds < 3600:
+            return f"{int(delta_seconds / 60)}m"
+        elif delta_seconds < 86400:
+            return f"{int(delta_seconds / 3600)}h"
+        else:
+            return f"{int(delta_seconds / 86400)}d"
