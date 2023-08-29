5ea75319cd5272ea18800ca0543750821c802474
EarthCaspian <hazar.a@outlook.com>
2023-08-29 18:46:53 +0300

    hesap bilgilerine ulasmak icin context processor eklentisi

--- user/context_processors.py
+++ user/context_processors.py
@@ -6,3 +6,10 @@ def get_profiles(request):
         'profiller':profiller
     }
     return context
+
+def get_hesap(request):
+    hesapBilgi = Hesap.objects.filter(user = request.user) if request.user.is_authenticated else ''
+    context = {
+        'hesapBilgi' : hesapBilgi,
+    }
+    return context
